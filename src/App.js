1ec10be67f6ff19bc3f42665d97d45c229bb083c
Your Name <fontematheus454@gmail.com>
2023-11-23 16:36:22 -0300

    trabalho

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
-import{ useState } from 'react';
+import { useState } from 'react';
 import './styles.css';
 
 import api from './services/api';
 
 function App() {
 
- const [input, setInput] = useState('')
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({});
 
-async function handleSearch(){
-  // 89220700/json/
 
-  if(input === ''){
-    alert("Preencha algum cep")
-    return;
+  async function handleSearch() {
+    // 89220700/json/
+
+    if (input === '') {
+      alert("Preencha algum cep")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("");
+
+    } catch {
+      alert("Ops! erro ao buscar");
+      setInput("")
+    }
+
   }
-
-  try{
-    const response = await api.get(`${input}/json`);
-    console.log(response.data)
-
-  }catch{
-alert("Ops! erro ao buscar");
-  }
-
-}
 
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
 
-       <div className="containerInput">
-        <input 
-        type="text"
-        placeholder="Digite seu cep..."
-        value={input}
-        onChange={(e) => setInput (e.target.value) }
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
 
         <button className="buttonSearch" onClick={handleSearch}
         >Pesquisar
 
         </button>
-       </div>
-
-       <main className="main">
-        <h2>CEP: 79003222</h2>
-
-        <span>Rua Teste algum</span>
-        <span>Complemento: Algum</span>
-        <span>Víla Rosa</span>
-        <span>Campo Grande - MS</span>
+      </div>
 
 
-       </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep} </h2>
+
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+
+        </main>
+
+      )}
+
 
     </div>
   );
